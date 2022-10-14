cac30754c927e7ca70cebfeeb0084885277c50df
ajohnsoon <allanjohnson.leal@gmail.com>
2022-10-14 20:36:06 -0300

    feat: add enum orderStatus to order

--- src/main/java/com/webservice/application/config/TestConfig.java
+++ src/main/java/com/webservice/application/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.webservice.application.config;
 
 import com.webservice.application.entities.Order;
 import com.webservice.application.entities.User;
+import com.webservice.application.entities.enums.OrderStatus;
 import com.webservice.application.repositories.OrderRepository;
 import com.webservice.application.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,10 +28,10 @@ public class TestConfig implements CommandLineRunner {
         User user2 = new User(null, "Bob Blue", "boblue@gmail.com", "9123456789", "13248aeiou");
         User user3 = new User(null, "Alex Green", "alexgreen@gmail.com", "944446666", "1a2b3c4d");
 
-        Order order1 = new Order(null, Instant.parse("2022-06-20T19:53:07Z"), user1);
-        Order order2 = new Order(null, Instant.parse("2022-10-14T12:22:35Z"), user3);
-        Order order3 = new Order(null, Instant.parse("2022-09-20T16:35:44Z"), user1);
-        Order order4 = new Order(null, Instant.parse("2022-07-08T12:13:58Z"), user2);
+        Order order1 = new Order(null, Instant.parse("2022-06-20T19:53:07Z"), OrderStatus.PAID, user1);
+        Order order2 = new Order(null, Instant.parse("2022-10-14T12:22:35Z"), OrderStatus.WAITING_PAYMENT,user3);
+        Order order3 = new Order(null, Instant.parse("2022-09-20T16:35:44Z"), OrderStatus.PAID,user1);
+        Order order4 = new Order(null, Instant.parse("2022-07-08T12:13:58Z"), OrderStatus.DELIVERED ,user2);
 
         userRepository.saveAll(Arrays.asList(user1,user2, user3));
         orderRepository.saveAll(Arrays.asList(order1, order2, order3, order4));
