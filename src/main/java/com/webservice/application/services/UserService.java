66284f83d94cbaec9ee3607d4a9b1de29f1193ab
ajohnsoon <allanjohnson.leal@gmail.com>
2022-10-14 10:59:43 -0300

    feat: add service layer and component registration

---
+++ src/main/java/com/webservice/application/services/UserService.java
@@ -0,0 +1,25 @@
+package com.webservice.application.services;
+
+import com.webservice.application.entities.User;
+import com.webservice.application.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> userObject = userRepository.findById(id);
+        return userObject.get();
+    }
+}
