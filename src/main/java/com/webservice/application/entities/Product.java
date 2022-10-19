e5a3b896910b57b9b3af4f7b578f0cde197e4b73
ajohnsoon <allanjohnson.leal@gmail.com>
2022-10-19 19:58:36 -0300

    feat: add product order item one-to-many association

--- src/main/java/com/webservice/application/entities/Product.java
+++ src/main/java/com/webservice/application/entities/Product.java
@@ -1,5 +1,6 @@
 package com.webservice.application.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -30,6 +31,9 @@ public class Product implements Serializable {
             inverseJoinColumns = @JoinColumn(name = "category_id")
     )
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product() {}
 
@@ -87,6 +91,15 @@ public class Product implements Serializable {
 
     public void setCategories(Set<Category> categories) {
         this.categories = categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for(OrderItem element : items){
+            set.add(element.getOrder());
+        }
+        return set;
     }
 
     @Override
