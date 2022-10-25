55cfd76485340a36a7f9a021f60656799cdf919a
ajohnsoon <allanjohnson.leal@gmail.com>
2022-10-24 22:34:26 -0300

    feat: user insert method

--- src/main/java/com/webservice/application/resources/UserResource.java
+++ src/main/java/com/webservice/application/resources/UserResource.java
@@ -1,15 +1,22 @@
 package com.webservice.application.resources;
 
 import com.webservice.application.services.UserService;
+
+import jakarta.servlet.Servlet;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.webservice.application.entities.User;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -28,6 +35,13 @@ public class UserResource {
     public ResponseEntity<User> findById(@PathVariable Long id) {
         User user = userService.findById(id);
         return ResponseEntity.ok().body(user);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = userService.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 
 }
