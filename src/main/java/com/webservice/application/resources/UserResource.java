418746c99aa54d7fc488e23f020e59e5ff11aa85
ajohnsoon <allanjohnson.leal@gmail.com>
2022-10-25 18:26:40 -0300

    feat: add method to delete a user with possible exception

--- src/main/java/com/webservice/application/resources/UserResource.java
+++ src/main/java/com/webservice/application/resources/UserResource.java
@@ -2,10 +2,9 @@ package com.webservice.application.resources;
 
 import com.webservice.application.services.UserService;
 
-import jakarta.servlet.Servlet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,4 +43,9 @@ public class UserResource {
         return ResponseEntity.created(uri).body(obj);
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.ok().build();
+    }
 }
